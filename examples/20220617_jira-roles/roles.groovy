83ca481f0c6a759f091dee2450647eb9c67de291
zhang <zhangqianli@live.cn>
2022-06-17 16:54:26 +0800

    jira用户角色

---
+++ examples/20220617_jira-roles/roles.groovy
@@ -0,0 +1,22 @@
+import com.atlassian.jira.ComponentManager
+import com.atlassian.jira.project.Project
+import com.atlassian.jira.project.ProjectManager
+import com.atlassian.jira.security.roles.ProjectRole
+import com.atlassian.jira.security.roles.ProjectRoleActors
+import com.atlassian.jira.security.roles.ProjectRoleManager
+
+if (ComponentAccessor.getGroupManager().getGroupsForUser(currentUser)?.find { it.name == "jira-administrators" }) {
+    return false
+}
+
+ComponentManager componentManager = ComponentManager.getInstance()
+ProjectManager projectManager = componentManager.getProjectManager()
+ProjectRoleManager projectRoleManager =  ComponentAccessor.getComponent(ProjectRoleManager)
+
+// name of role here
+ProjectRole adminRole = projectRoleManager.getProjectRole("Project Administrators")
+def currentUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+
+if (projectRoleManager.isUserInProjectRole(currentUser, adminRole, underlyingIssue.getProjectObject())) {
+    return false
+}
